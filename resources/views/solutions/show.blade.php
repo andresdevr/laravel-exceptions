712cd6ffb03f80d734c2a083d40c182f0d6b7e16
andresdevr <andres22.dev@gmail.com>
2021-09-14 18:12:53 +0000

    views

---
+++ resources/views/solutions/show.blade.php
@@ -0,0 +1,25 @@
+<x-exceptions-exceptions-layout>
+    <x-exceptions-breadcrumb>
+        <x-exceptions-breadcrumb-item route="{{ route(config('laravel-exceptions.route-prefix-name') . 'exceptions.index') }}">
+            {{ __('Exceptions') }}
+        </x-exceptions-breadcrumb-item>
+	<x-exceptions-breadcrumb-item route="{{ route(config('laravel-exceptions.route-prefix-name') . 'exceptions.show', ['exception' => $error->exception->id]) }}">
+            {{ $error->exception->id }}
+        </x-exceptions-breadcrumb-item>
+	<x-exceptions-breadcrumb-item route="{{ route(config('laravel-exceptions.route-prefix-name') . 'exceptions.show', ['exception' => $error->exception->id]) }}">
+            {{ __('Errors') }}
+        </x-exceptions-breadcrumb-item>
+	<x-exceptions-breadcrumb-item route="{{ route(config('laravel-exceptions.route-prefix-name') . 'exceptions.errors.show', ['exception' => $error->exception->id, 'error' => $error->id]) }}">
+            {{ $error->id }}
+        </x-exceptions-breadcrumb-item>
+	<x-exceptions-breadcrumb-item route="{{ route(config('laravel-exceptions.route-prefix-name') . 'errors.solutions.create', ['error' => $error->id]) }}">
+            {{ __('Solutions') }}
+        </x-exceptions-breadcrumb-item>
+	<x-exceptions-breadcrumb-item route="{{ route(config('laravel-exceptions.route-prefix-name') . 'errors.solutions.show', ['error' => $error->id, 'solution' => $solution->id]) }}">
+            {{ $solution->id }}
+        </x-exceptions-breadcrumb-item>
+    </x-exceptions-breadcrumb>
+    <div class="my-3">
+        
+    </div>
+</x-exceptions-exceptions-layout>
